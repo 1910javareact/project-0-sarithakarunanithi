4df394ebbc8eb487e81da32e28a991b29c813e6d
sarithakarunanithi <sarithakumar06@gmail.com>
2019-11-20 16:40:56 -0500

    Models Created

---
+++ src/models/user.ts
@@ -0,0 +1,22 @@
+// The User model keeps track of users information
+
+export class User{
+          userId: number     // primary key
+          username: string   // not null, unique
+          password: string   // not null
+          firstName: string  // not null
+          lastName: string   // not null
+          email: string      // not null
+          role: string[]     // not null
+
+          constructor(userId:number,username:string,password:string,firstname:string,
+                      lastName:string,email:string,role:string[]){
+                          this.userId = userId
+                          this.username = username
+                          this.password = password
+                          this.firstName = firstname
+                          this.lastName = lastName
+                          this.email = email
+                          this.role = role
+          }
+}
